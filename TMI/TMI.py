1b46ce9f204a5caab710551824aabef4335114d0
Clarke <ssclarke@ndc.nasa.gov>
2025-03-06 16:58:24 -0800

    Changing app.py to have simple call to each tool. Inference code will happen in director for model instead of app.py

---
+++ TMI/TMI.py
@@ -0,0 +1,14 @@
+def predict_tmi(currentStatus, request):
+	try:
+		if currentStatus == "GDP":
+			tmi_pred = "GDP"
+		elif currentStatus == "GS":
+			tmi_pred = "GS",
+		else:
+			tmi_pred = "No TMI"
+	except:
+		return "Error"
+	
+	return tmi_pred
+
+	# print("returning: " + results['text'])
